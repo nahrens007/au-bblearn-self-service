a5bc5ad0b053b29810b04de1ab9d1ca5c76c39d5
Nathan Ahrens <nathanmahrens@gmail.com>
2018-01-28 15:42:48 -0500

    stuff

--- bblearn/static.py
+++ bblearn/static.py
@@ -1,3 +1,5 @@
+'''
+
 # Erich
 
 key = 'f3f2402e-9717-45d5-ba37-67d03326c9b0'
@@ -9,7 +11,8 @@ server = 'https://127.0.0.1:9877'
 key = "0de73b0f-68e7-48cc-b1e9-0fbefe209b74"
 secret = "2Fz6vO82Y035LfoYxlQrY7xyV21SAYPK"
 server = 'https://localhost:9877'
-
+'''
 # Laptop
 key = "45ff63ad-e072-42fb-b6af-3c33bff846b4"
 secret = "cQXOMrwe0pxMaTQbnozmUvjCxzeGSbKM"
+server = 'https://localhost:9877'
