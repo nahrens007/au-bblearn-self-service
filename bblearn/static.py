00011bbd68e59a0264117ac6ed3f82ec9d864083
Nathan Ahrens <nathanmahrens@gmail.com>
2018-01-28 13:15:42 -0500

    reorganizing

---
+++ bblearn/static.py
@@ -0,0 +1,15 @@
+# Erich
+
+key = 'f3f2402e-9717-45d5-ba37-67d03326c9b0'
+secret = 'dxy0ynHs2ymwXeGIi3rM1BZzeNfqLXK5'
+server = 'https://127.0.0.1:9877'
+
+
+# Desktop
+key = "0de73b0f-68e7-48cc-b1e9-0fbefe209b74"
+secret = "2Fz6vO82Y035LfoYxlQrY7xyV21SAYPK"
+server = 'https://localhost:9877'
+
+# Laptop
+key = "45ff63ad-e072-42fb-b6af-3c33bff846b4"
+secret = "cQXOMrwe0pxMaTQbnozmUvjCxzeGSbKM"
