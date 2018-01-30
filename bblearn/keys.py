5d300f7e3784ce76b6f0e366c640de914993dd75
Nathan Ahrens <nathanmahrens@gmail.com>
2018-01-30 09:44:27 -0500

    Nate's laptop

--- bblearn/keys.py
+++ bblearn/keys.py
@@ -1,10 +1,11 @@
-
+'''
 
 # Erich's Macbook
 key = 'f3f2402e-9717-45d5-ba37-67d03326c9b0'
 secret = 'dxy0ynHs2ymwXeGIi3rM1BZzeNfqLXK5'
 server = 'https://127.0.0.1:9877'
 
+'''
 
 '''
 # Nathan's Desktop
@@ -12,14 +13,14 @@ key = "0de73b0f-68e7-48cc-b1e9-0fbefe209b74"
 secret = "2Fz6vO82Y035LfoYxlQrY7xyV21SAYPK"
 server = 'https://localhost:9877'
 '''
-'''
+
 
 # Nathan's Laptop
 key = "45ff63ad-e072-42fb-b6af-3c33bff846b4"
 secret = "cQXOMrwe0pxMaTQbnozmUvjCxzeGSbKM"
 server = 'https://localhost:9877'
 
-'''
+
 
 '''
 
