3255c7dc06d4d5c218619936145bf9ae44931a9d
Nathan Ahrens <nathanmahrens@gmail.com>
2018-02-01 18:15:10 -0500

    listed users where username match a search string

--- bblearn/learn/views.py
+++ bblearn/learn/views.py
@@ -87,6 +87,86 @@ def viewUsers(request):
     return render(request, 'learn/viewUsers.html', context)
 
 def addUsers(request):
+
+    #if request.method == "POST":
+    if True:
+        #courses = request.POST.get('courses') # returns None if courses was not posted
+        #search = request.POST.get('search') # contain searchKey and searchString
+        #will replace with data from search
+        searchKey = 'userName'
+        searchString = 'ba'.lower()
+
+        path = '/learn/api/public/v1/users?fields=userName,name,contact,studentId,availability'
+        r = interface.get(path)
+        if r == None:
+            #This could be caused when either the server url is incorrect or Python can't connect to Bb at all
+            return render(request, 'learn/addUsers.html', { 'error_message' : 'Could not connect to Blackboard!' })
+        elif r.status_code == 200:
+            #Success!
+
+            userList = ''
+            if r.text:
+                res = json.loads(r.text)
+
+                users = res['results']
+
+                # build list of users to display
+                for user in users:
+                    if 'availability' in user and user['availability']['available'] == 'Yes' and searchString in user[searchKey]:
+                        userList += '<tr>'
+                        userList += '<td><input class="userCheckbox" type="checkbox" ></td>'
+                        if 'userName' in user:
+                            userList += '<td>' + user['userName'] + '</td>'
+                        else:
+                            userList += '<td></td>'
+                        if 'name' in user:
+                            userList += '<td>' + user['name']['given'] + '</td>'
+                            userList += '<td>' + user['name']['family'] + '</td>'
+                        else:
+                            userList += '<td></td>'
+                            userList += '<td></td>'
+                        if 'contact' in user:
+                            userList += '<td>' + user['contact']['email'] + '</td>'
+                        else:
+                            userList += '<td></td>'
+                        if 'studentId' in user: #guests don't have studentId
+                            userList += '<td>' + user['studentId'] + '</td>'
+                        else:
+                            userList += '<td></td>'
+                        userList += '</tr>'
+
+            if userList == '':
+                context = {
+                    'name': 'From Cookies',
+                    'error_message':"No users found!",
+                    'userList': '',
+                }
+                return render(request, 'learn/addUsers.html', context)
+
+            context ={
+                'name': 'From Cookies',
+                'error_message': '',
+                'userList': userList,
+            }
+
+            return render(request, 'learn/addUsers.html', context)
+
+        elif r.status_code == 403:
+            return render(request, 'learn/addUsers.html', { 'error_message' : 'You are not authorized!' })
+        elif r.status_code == 400:
+            return render(request, 'learn/addUsers.html', { 'error_message' : 'Bad request!' })
+        elif r.status_code == 401:
+            return render(request, 'learn/addUsers.html', { 'error_message' : 'There was a Blackboard authentication error!' })
+        else:
+            print("[DEBUG] r.status_code for courses get(): " + str(r.status_code))
+    else: # regular index : sign in page
+        context ={
+            'name': 'From Cookies',
+            'error_message': '',
+            'userList': '',
+        }
+        return render(request, 'learn/addUsers.html', context)
+
     context = {
 
     }
