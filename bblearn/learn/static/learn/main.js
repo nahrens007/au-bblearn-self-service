798f4136eb1a182a7062e791807eb46c04ab7dec
LordBard42 <eberger3@ashland.edu>
2018-02-05 17:30:47 -0500

    Edited Add Users

--- bblearn/learn/static/learn/main.js
+++ bblearn/learn/static/learn/main.js
@@ -3,7 +3,7 @@
 function check() {
 
   isChecked = document.getElementById('checkAll').checked;
-
+  print("working");
   if(isChecked){
     document.getElementsByClassName('userCheckbox').checked = true;
   }
