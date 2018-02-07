08675324105e2ea8fd7957e905b1bdcdf1aee714
Nathan Ahrens <nathanmahrens@gmail.com>
2018-02-06 19:38:37 -0500

    changes in post data

--- bblearn/learn/addusers.py
+++ bblearn/learn/addusers.py
@@ -19,20 +19,21 @@ def addUsers(request):
     # WAY WE GET HERE IS FROM THE update() VIEW, WHICH VERIFIES POST
     user = request.POST.getlist('user')
     course = request.POST.getlist('course')
-    searchBy = request.POST.getlist('searchBy')
-    searchBar = request.POST.getlist('searchBar')
-
+    #searchBy = request.POST.getlist('searchBy')
+    #searchBar = request.POST.getlist('searchBar')
+    searchKey = str(request.POST.get('searchBy')).lower() # if searchKey is name (first/last), further configuration will be needed.
+    searchString = str(request.POST.get('searchBar')).lower()
     print()
     print('user: ' + str(user))
     print('course: ' + str(course))
-    print('searchBy: ' + str(searchBy))
-    print('searchBar: ' + str(searchBar))
+    print('searchKey: ' + searchKey)
+    print('searchString: ' + searchString)
     print()
     #search = request.POST.get('search') # contain searchKey and searchString
     #will replace with data from search
-    searchKey = 'email' # if searchKey is name (first/last), further configuration will be needed.
+    #searchKey = 'email' # if searchKey is name (first/last), further configuration will be needed.
 
-    searchString = 'ba'.lower()
+    #searchString = 'ba'.lower()
 
     path = '/learn/api/public/v1/users?fields=userName,name.given,name.family,contact.email,studentId,availability'
     r = interface.get(path)
