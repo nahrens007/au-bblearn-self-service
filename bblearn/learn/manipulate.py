cb0415f7d05590643964e5cf66c07f4bd2cae2d1
bdouglas22 <bdouglas@ashland.edu>
2018-02-09 08:51:04 -0500

    Finally....

--- bblearn/learn/manipulate.py
+++ bblearn/learn/manipulate.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
+from BlackboardLearn import interface
+import json
 
 def viewUsers(request):
+    courses = request.POST.getlist('course')
+
+
+    for course in courses:
+
+        "Gets all users from course"
+        path = "/learn/api/public/v1/courses/"+course+"/users"
+        r = interface.get(path)
+
+        if r.text
+
+            res = json.loads(r.text)
+            "Grabs all the userId's from the course"
+            members = res['results'][userId]
+
+            for member in members:
+
+                "Grabs individual user information to display"
+                path = "/learn/api/public/v1/users/"+member
+                r = interface.get(path)
+
+                if r.text
+
+                    res = json.loads(r.text)
+
+                    fName = res['name'][given]
+                    lName = res['name'][family]
+                    email = res['contact'][email]
+                    idNumber = res[studentId]
+
+                "Gets current user's role in the course"
+                path = "GET /learn/api/public/v1/courses/"+course+"/users/"+member
+                r = interface.get(path)
+
+                if r.text
+
+                    res = json.loads(r.text)
+                    courseRole = res[courseRoleId]
+
     context = {
 
     }
