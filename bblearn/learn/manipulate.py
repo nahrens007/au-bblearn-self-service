f7aeed7fcdc777c53a329a0c0348fc86425de755
bdouglas22 <bdouglas@ashland.edu>
2018-02-14 20:22:14 -0500

    Happy Valentines Day Peeps!

--- bblearn/learn/manipulate.py
+++ bblearn/learn/manipulate.py
@@ -59,7 +59,7 @@ def viewUsers(request):
     }
     return render(request, 'learn/viewUsers.html', context)
 
-'''def removeUsers(request):
+def removeUsers(request):
 
     courses = request.POST.getlist('course')
 
@@ -104,33 +104,33 @@ def viewUsers(request):
                 for member in members:
 
                     "Gets current user's role in the course"
-                    path = "GET /learn/api/public/v1/courses/"+course+"/users/"+member
+                    path = "/learn/api/public/v1/courses/"+course+"/users/"+member['userId']
                     r = interface.get(path)
 
                     if r.text:
                         res = json.loads(r.text)
-                        courseRole = res[courseRoleId]
+                        courseRole = res['courseRoleId']
 
                         '''Checks for all teaching assistants and guests in the selected courses'''
                         if(courseRole == 'TeachingAssistant'):
                             tableCreator += buildRemoveList(course, member['userId'], 'Teaching Assistant')
                         elif(courseRole == 'Guest'):
                             tableCreator += buildRemoveList(course, member['userId'], 'Guest')
-                        tableCreator += '<tr id="submitRow">'
-                        tableCreator += '<td><input id="checkAll" type="checkbox" onclick="check()" ></td>'
-                        tableCreator += '<td></td>'
-                        tableCreator += '<td></td>'
-                        tableCreator += '<td></td>'
-                        tableCreator += '<td><input id="submit" type="button" value="Remove"></td>'
-                        tableCreator += '</tr>'
-                        tableCreator += '</table>'
-                        tableCreator += '</div>'
+                tableCreator += '<tr id="submitRow">'
+                tableCreator += '<td><input id="checkAll" type="checkbox" onclick="check()" ></td>'
+                tableCreator += '<td></td>'
+                tableCreator += '<td></td>'
+                tableCreator += '<td></td>'
+                tableCreator += '<td><input id="submit" type="button" value="Remove"></td>'
+                tableCreator += '</tr>'
+                tableCreator += '</table>'
+                tableCreator += '</div>'
     context = {
     'name': request.session['instructor_name'],
     'tableCreator': tableCreator,
     }
 
-    return render(request, 'learn/removeUsers.html', context)'''
+    return render(request, 'learn/removeUsers.html', context)
 
 def buildViewList(course, member):
 
